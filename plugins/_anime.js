b2f20e1f8363e9a39f42aef15d30eb7f506ea6d0
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2024-03-15 16:05:01 +0300

    Update _anime.js

--- plugins/_anime.js
+++ plugins/_anime.js
@@ -27,6 +27,6 @@ let handler = async (m, { command, text }) => m.reply(`*╔═╼─╾═【�
 
 handler.help = ['الاوامر <teks>?']
 handler.tags = ['الاوامر', 'fun']
-handler.command = /^(الاستماره|1|استماره\استمارة)$/i
+handler.command = /^(الاستماره|استماره|استمارة)$/i
 
 export default handler
