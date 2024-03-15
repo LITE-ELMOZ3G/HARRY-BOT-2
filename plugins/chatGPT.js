5241b699d1520f6182d686df24b87e87e4631a1c
Raedxx1 <149210006+Raedxx1@users.noreply.github.com>
2024-03-15 19:56:32 +0300

    Update chatGPT.js

--- plugins/chatGPT.js
+++ plugins/chatGPT.js
@@ -2,17 +2,17 @@ import fetch from  'node-fetch'
 let handler = async (m, { text, usedPrefix, command }) => {
 if (!text) throw `لوفي اخيرا اصبح يدعم ميزة الذكاء الاصطناعي: يمكنك الان البحث عن البحوث والعروض والترجمة والاجابة عن التمارين والأسئلة كيف ما كان نوعها و بكل اللغات \n \n`
 try {
-let IA2 = await fetch(`https://api.dify.ai/v1`)  
+let IA2 = await fetch(`app-ZQE5HgZQhWXKTU89H2uAFsIH`)  
 let IAR2 = await IA2.json()
 m.reply(`${IAR2.response}`.trim())    
 } catch {
 try {   
-let rrEes = await fetch(`https://api.dify.ai/v1`)
+let rrEes = await fetch(`app-ZQE5HgZQhWXKTU89H2uAFsIH`)
 let jjJson = await rrEes.json()
 m.reply(jjJson.data.data.trim())    
 } catch {      
 try {    
-let tioress = await fetch(`https://api.dify.ai/v1`)
+let tioress = await fetch(`app-ZQE5HgZQhWXKTU89H2uAFsIH`)
 let hasill = await tioress.json()
 m.reply(`${hasill.result}`.trim())   
 } catch {        
